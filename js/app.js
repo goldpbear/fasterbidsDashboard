4d00914d2145d8e77984e80428715967fa1de669
goldpbear <tcroxson@gmail.com>
2016-04-11 15:00:11 -0700

    add labels

--- js/app.js
+++ js/app.js
@@ -14,12 +14,37 @@ FASTERBIDS.controller("dashboardCtrl", function($scope) {
 	// TODO: api will be called here
 	d3.json("data/data.json", function(err, data) {
 		if (err) { throw err; }
-		$scope.data = data;
-		
+		$scope.data = data.data;
 		$scope.metadata = data.metadata;
-		$scope.$apply();
 
+		$scope.selectedFilters = [];
+		$scope.metadata.forEach(function(metadata, index){
+			$scope.selectedFilters.push(
+				{
+					"name": metadata.name,
+					"value": metadata.items[0]
+				}
+			);
+		});
+
+		$scope.updateData();
+		$scope.$apply();
 	});
+
+	$scope.onInputChange = function(value, name, index) {
+		$scope.selectedFilters[index].name = name;
+		$scope.selectedFilters[index].value = value;
+		$scope.updateData();
+	}
+
+	$scope.updateData = function() {
+		$scope.filteredData = $scope.data;
+		$scope.selectedFilters.forEach(function(filter) {
+			$scope.filteredData = $scope.filteredData.filter(function(item, n) {
+				return item[filter.name] == filter.value;
+			});
+		});
+	}
 });
 
 
@@ -36,31 +61,43 @@ FASTERBIDS.directive("pieChart", function() {
 					.attr('height', height)
 					.append('g')
 					.attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')'),
-			// dummy data
-			data = [21, 32, 35, 64, 83],
-			pie = d3.layout.pie(),
+			data = [],
+			pie = d3.layout.pie()
+					.value(function(d) { return d.sales }),
 			arcData = pie(data),
-			arc = d3.svg.arc().outerRadius(radius - padding),
-			path = svg.selectAll("path")
-					  .data(arcData)
-					  .enter()
-					  .append("path")
-					  .attr("d", arc)
-					  .attr("fill", function(d, i) { 
-					    return color(i);
-					  });
+			arc = d3.svg.arc()
+					.outerRadius(radius - padding)
+					.innerRadius(0),
+			arcs = svg.selectAll("g.arc");
+			//path = svg.selectAll("path");
 
-		scope.$watch("data", function(data) {
-			//path.data(pie(data)).attr("d", arc);
-			console.log("data updated!", data);
+		// update the pie chart when the data changes
+		scope.$watch("filteredData", function(filteredData) {
+			filteredData = filteredData || [];
+			arcs.data(pie(filteredData))
+				.enter()
+				.append("g")
+				.append("path")
+			    .attr("fill", function(d, i) {
+			    	return color(i);
+			    })
+			    .attr("d", arc);
+
+			svg.selectAll("g")
+			    .append("text")
+			    .attr("transform", function(d) {
+			    	return "translate(" + arc.centroid(d) + ")";
+			    })
+			    .attr("text-anchor", "middle")
+			    .text(function(d) {
+			    	console.log(d.data.region);
+			    	return d.data.region;
+			    });
 		}, true);
-
 	};
 
 	return {
 		link: link,
 		restrict: "E"
 	}
-
-
 });
